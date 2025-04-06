264d3069a74abbf6f149662b628a34ec2f6f8ff3
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2025-04-06 19:49:21 +0200

    remount

--- mount.sh
+++ mount.sh
@@ -1,2 +1,5 @@
 . /home/pi/paperless/.blobfuse-env.sh
-blobfuse2 --allow-other --config-file=/home/pi/paperless/blobFuseConfig.yaml /mnt/paperless-backup/
+if ! mountpoint -q /mnt/paperless-backup/; then
+    echo "paperless mount point not mounted, mounting..."
+    blobfuse2 --allow-other --config-file=/home/pi/paperless/blobFuseConfig.yaml /mnt/paperless-backup/
+fi
